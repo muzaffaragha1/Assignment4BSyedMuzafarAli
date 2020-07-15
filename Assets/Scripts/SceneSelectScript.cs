d510c223fea334b6566f07a330b363135f807073
Syed Muzafar Ali <smuzaffaragha@gmail.com>
2020-07-16 02:38:03 +0500

    My Initial Commit

---
+++ Assets/Scripts/SceneSelectScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSelectScript : MonoBehaviour
+{
+    public void SelectScene()
+    {
+        switch (this.gameObject.name)
+        {
+            case "PenguinButton": SceneManager.LoadScene("Penguins");
+                break;
+            
+            case "HummingbirdButton": SceneManager.LoadScene("FlowerIsland");
+                break;
+            
+        }
+    }
+}
